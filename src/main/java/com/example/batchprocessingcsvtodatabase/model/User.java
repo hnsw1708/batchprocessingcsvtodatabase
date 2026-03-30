797bd4f6a938942937ed122d784b2554360f9d7a
anupam1708 <anupam.1708@gmail.com>
2026-03-29 22:09:43 -0500

    fix: Upgrade Java 11 → 21 (LTS): applied OpenRewrite recipes (org.openrewrite.java.migrate.UpgradeToJava2

--- src/main/java/com/example/batchprocessingcsvtodatabase/model/User.java
+++ src/main/java/com/example/batchprocessingcsvtodatabase/model/User.java
@@ -1,13 +1,10 @@
 package com.example.batchprocessingcsvtodatabase.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Type;
-import org.hibernate.type.UUIDCharType;
-import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -15,24 +12,16 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "`user`")
-@Builder
-public class User extends BaseEntity{
+@Table(name = "user")
+public class User extends BaseEntity {
 
-    @Type(UUIDCharType.class)
     private UUID personId;
-
     private String firstName;
     private String lastName;
     private String email;
-
     @Enumerated(EnumType.STRING)
     private Gender gender;
-
     private String country;
-
-    @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate birthday;
-
-    private int age;
+    private Integer age;
 }
