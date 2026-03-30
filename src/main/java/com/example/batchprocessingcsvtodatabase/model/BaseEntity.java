e68153cd1c3d3903ce483dbc56264181e9966953
anupam1708 <anupam.1708@gmail.com>
2026-03-30 11:16:16 -0500

    fix: Upgrade Java 11 → 21 (LTS): applied OpenRewrite recipes (org.openrewrite.java.migrate.UpgradeToJava2

--- src/main/java/com/example/batchprocessingcsvtodatabase/model/BaseEntity.java
+++ src/main/java/com/example/batchprocessingcsvtodatabase/model/BaseEntity.java
@@ -2,10 +2,10 @@ package com.example.batchprocessingcsvtodatabase.model;
 
 import lombok.Getter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Getter
